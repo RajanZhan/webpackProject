9002cbd6b64af5a716aa0f1349047d1ea5f8bd2c
rajan <rajan.zhan@foxmail.com>
2018-04-20 21:28:29 +0800

    修复bug

--- bin/task.cmd
+++ bin/task.cmd
@@ -6,7 +6,6 @@ copy %cd%\config.json %cd%\build\config.json /A
 Xcopy %cd%\static %cd%\build\static\ /S /A /Y
 Xcopy %cd%\dist %cd%\build\dist\ /S /A /Y
 Xcopy %cd%\dataModel %cd%\build\dataModel\ /S /A /Y
-Xcopy %cd%\common %cd%\build\common\ /S /A /Y
 echo "请运行install.cmd 安装依赖包，并运行build.js 启动系统" > build/readme.txt
 echo "compile finished....OK"
 pause
